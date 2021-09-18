ae44866adbffe73468722985c6130ac37179bc2f
Potatoes <cd64@mail.com>
2021-09-17 22:21:35 -0400

    change plugin info

--- src/Plugin/src/PluginInfo.cs
+++ src/Plugin/src/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace H3VRMod
 {
 	internal static class PluginInfo
 	{
-		internal const string NAME = "Example H3VR Mod";
-		internal const string GUID = "com.yourname.h3vrmod";
+		internal const string NAME = "Twin Stick Swinger";
+		internal const string GUID = "dll.h3vr.potatoes.twinstickswinger";
 		internal const string VERSION = "1.0.0";
 	}
 }
