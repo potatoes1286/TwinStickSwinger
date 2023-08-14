8ef542207e744348b6c4c480bf855a1d5c1f8e75
Potatoes <48143760+potatoes1286@users.noreply.github.com>
2023-08-14 19:26:38 -0400

    Bump to 1.0.1

--- src/TSS/src/PluginInfo.cs
+++ src/TSS/src/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace H3VRMod
 	{
 		internal const string NAME = "Twin Stick Swinger";
 		internal const string GUID = "dll.h3vr.potatoes.twinstickswinger";
-		internal const string VERSION = "1.0.0";
+		internal const string VERSION = "1.0.1";
 	}
 }
