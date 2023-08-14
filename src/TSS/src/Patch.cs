6c80f4655b797028b2a9912f064074397653a84c
Potatoes <48143760+potatoes1286@users.noreply.github.com>
2023-08-14 19:26:25 -0400

    Fix mod, add comments, add nuget

--- src/TSS/src/Patch.cs
+++ src/TSS/src/Patch.cs
@@ -13,11 +13,21 @@ namespace H3VRMod
 		{
 			if (__instance.Mode == FVRMovementManager.MovementMode.Dash)
 			{
-				__instance.AXButtonCheck(hand);
+				//Save some states, set some necessary states
+				//Prevent turning by TwinStick when pressing Armswinger buttons
 				var tssts = GM.Options.MovementOptions.TwinStickSnapturnState;
 				GM.Options.MovementOptions.TwinStickSnapturnState = MovementOptions.TwinStickSnapturnMode.Disabled;
+				//Jumping for TwinStick is broken. I have NO fucking idea why.
+				var jumpstate = GM.Options.MovementOptions.TwinStickJumpState;
+				GM.Options.MovementOptions.TwinStickJumpState = MovementOptions.TwinStickJumpMode.Disabled;
+				
 				__instance.HandUpdateTwinstick(hand);
+				
+				//Re-set those states
+				GM.Options.MovementOptions.TwinStickJumpState = jumpstate;
 				GM.Options.MovementOptions.TwinStickSnapturnState = tssts;
+				
+				__instance.AXButtonCheck(hand);
 				return false;
 			}
 			return true;
@@ -29,19 +39,28 @@ namespace H3VRMod
 		{
 			if (__instance.Mode == FVRMovementManager.MovementMode.Dash)
 			{
-				//set gravmode to 0 so that twinstick doesn't also simulate gravity again
+				//Okay so this is a little fucky!
+				
+				//Set gravity to zero. Gravity is calculated at UpdateSmoothLocomotion, and we're running it twice, so
+				//if we don't set grav to 0 we will simulate grav twice.
 				var gravmode = GM.Options.SimulationOptions.PlayerGravityMode;
 				GM.Options.SimulationOptions.PlayerGravityMode = SimulationOptions.GravityMode.None;
-				__instance.UpdateModeTwoAxis(true);
+				
+				//Set to twinstick and run USL. USL here wont sim grav + will sim twinstick
+				__instance.Mode = FVRMovementManager.MovementMode.TwinStick;
+				__instance.UpdateSmoothLocomotion();
+				
+				//Reset gravity.
 				GM.Options.SimulationOptions.PlayerGravityMode = gravmode;
-				//once it's done, set mode for armswinger and let the original method do its calcs
+				
+				//Set to armswinger.
 				__instance.Mode = FVRMovementManager.MovementMode.Armswinger;
+				//Inform state that we're going to reset it back to Dash
 				__state = true;
 			}
 			else
-			{
-				__state = false;
-			}
+				__state = false; //We're not Dash (TSS). Just ignore everything and move on.
+			//If set to armswinger, it'll run USL again in the regular FU function, and simulate gravity properly.
 			return true;
 		}
 		
@@ -49,32 +68,19 @@ namespace H3VRMod
 		[HarmonyPostfix]
 		public static void Patch_MovementMathUpdateEnd(FVRMovementManager __instance, bool __state)
 		{
+			//Afterwards, reset the movement style to Dash so we can repeat the entire process.
 			if (__state)
-			{
 				__instance.Mode = FVRMovementManager.MovementMode.Dash;
-			}
 		}
 		
-		//[HarmonyPatch(typeof(FVRPointableButton), "Awake")]
-		[HarmonyPatch(typeof(FVRPointable), "Update")]
+		[HarmonyPatch(typeof(FVRPointableButton), "Awake")]
 		[HarmonyPostfix]
-		public static void Patch_FixDashName(FVRPointable __instance)
-		{
-			//WristMenu/MenuGo/Canvas/Button_LocoSet_0 (1)/Text
-			if (__instance.MaxPointingRange == 1)
-			{
-				__instance.MaxPointingRange = 1.001f;
-				if (__instance is FVRWristMenuPointableButton)
-				{
-					var text = __instance.GetComponentInChildren<Text>();
-					if (text != null)
-					{
-						if (text.text == "Dash")
-						{
-							text.text = "TSS";
-						}
-					}
-				}
+		//this just renames the Dash in the menu to TSS (Twin Stick Swinger)
+		public static void Patch_FixDashName(FVRPointableButton __instance) {
+			var text = __instance.GetComponent<Text>();
+			if (text != null) {
+				if (text.text == "Dash Teleport")
+					text.text = "Twin-Stick Swinger";
 			}
 		}
 	}
